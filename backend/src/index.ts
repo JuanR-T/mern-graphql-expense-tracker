e5cb976758eb47daa896c20ec3acb79f4fb82f71
JuanRT <juan.m13@hotmail.fr>
2024-12-20 09:59:03 +0100

    mongodb db init + schemas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,14 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 import http from 'http';
+import { connectDB } from './db/connecDB';
 import mergedResolvers from './resolvers';
 import mergedTypeDefs from './typeDefs';
 
+dotenv.config();
 const app = express();
 const httpServer = http.createServer(app);
 
@@ -30,3 +33,7 @@ app.use(
 await new Promise<void>((resolve) =>
     httpServer.listen({ port: 4000 }, resolve),
 );
+
+await connectDB();
+
+console.log(`Server ready at http://localhost:4000`);
