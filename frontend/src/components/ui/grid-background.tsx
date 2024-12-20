c4cca7cb775643ada6c2518e492796a004ef4356
JuanRT <juan.m13@hotmail.fr>
2024-12-20 22:40:20 +0100

    input + radioBtn components + SignUpPage

--- frontend/src/components/ui/grid-background.tsx
+++ frontend/src/components/ui/grid-background.tsx
@@ -1,11 +1,10 @@
-export function GridBackgroundDemo({ children }: { children: React.ReactNode }) {
+const GridBackground = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="h-screen w-full dark:bg-green-900 bg-green-100 dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="w-full dark:bg-green-900 bg-green-100 dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative">
             {/* Radial gradient for the container to give a faded look */}
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-green-800 bg-green-200 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-            <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-green-400 to-green-600 py-8">
-                {children}
-            </p>
+            {children}
         </div>
     );
 }
+export default GridBackground;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import { GridBackgroundDemo } from './components/ui/grid-background.tsx';
+import GridBackground from './components/ui/grid-background.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <GridBackgroundDemo >
+      <GridBackground >
         <App />
-      </GridBackgroundDemo>
+      </GridBackground>
     </BrowserRouter>
   </StrictMode>,
 )
