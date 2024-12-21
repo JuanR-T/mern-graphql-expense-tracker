708073e37de9cb05a7c3c6bf25257e1bb8e0cf14
JuanRT <juan.m13@hotmail.fr>
2024-12-21 15:08:50 +0100

    header component done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import SignUpPage from "./pages/SignupPage";
+import SignUpPage from "./pages/SignUpPage";
 import TransactionPage from "./pages/TransactionPage";
-import Header from "./components/Header";
 
 function App() {
   const authUser = true;
